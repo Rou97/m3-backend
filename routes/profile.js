c7c9d6b62b7fcb756e1bdfdd4bee54f9a682d5b0
Raul Delgado <raul@MacBook-Pro-de-Raul.local>
2019-03-27 16:38:58 +0100

    add route to profile

--- routes/profile.js
+++ routes/profile.js
@@ -5,5 +5,16 @@ const User = require('../models/user');
 
 const { isLoggedIn, isNotLoggedIn, validationLoggin } = require('../helpers/middlewares');
 
+// router.get('/', isLoggedIn(), (req, res, next) => {
+//     console.log('dentro');
+//     res.json(req.session.currentUser);
+//   });
+
+  router.get('/', isLoggedIn(), (req, res, next) => {
+    res.status(200).json({
+      message: 'This is a private message'
+    });
+  });
+
 
 module.exports = router;
